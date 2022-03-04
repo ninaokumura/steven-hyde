91dd0b7f7a4ef6ed95bdf1dc0ad57887b842b58f
Nina Okumura <elaineokumura@gmail.com>
2022-03-04 21:57:47 +1300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    fontFamily: {
+      sans: ["'Press Start 2P'", 'cursive'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
